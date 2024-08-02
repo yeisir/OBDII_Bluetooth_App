ef0659dee23a957b703d5c6f8d294b0e2d4d3590
Yeisi Rodriguez <78923067+yeisir@users.noreply.github.com>
2024-08-02 08:12:14 -0500

    Create Activity_Main.kt

---
+++ Activity_Main.kt
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="utf-8"?>
+<LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    xmlns:tools="http://schemas.android.com/tools"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:orientation="vertical"
+    tools:ignore="UseCompoundDrawables">
+
+    <ImageView
+        android:id="@+id/imageView"
+        android:layout_width="match_parent"
+        android:layout_height="166dp"
+        android:background="#E967FF"
+        android:contentDescription="LOGO"
+        android:src="@android:drawable/ic_dialog_map"
+        tools:ignore="ImageContrastCheck" />
+
+    <TextView
+        android:id="@+id/titulo"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_gravity="center_horizontal"
+        android:layout_marginTop="30dp"
+        android:fontFamily="serif-monospace"
+        android:text="GPS"
+        android:textColor="@color/black"
+        android:textSize="90sp"
+        android:textStyle="bold" />
+
+    <TextView
+        android:id="@+id/subtitulo"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_gravity="center_horizontal"
+        android:fontFamily="serif-monospace"
+        android:text="Enviar coordenadas"
+        android:textColor="@color/black"
+        android:textSize="30sp"
+        android:textStyle="bold" />
+
+    <Switch
+        android:id="@+id/switch1"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:scaleX="2"
+        android:scaleY="2"
+        android:layout_gravity="center_horizontal"
+        android:layout_marginTop="30dp" />
+
+    <Button
+        android:id="@+id/Enviar"
+        android:layout_width="153dp"
+        android:layout_height="59dp"
+        android:layout_gravity="center_horizontal"
+        android:layout_marginTop="30dp"
+        android:background="#F2A4FF"
+        android:fontFamily="monospace"
+        android:radius="30dp"
+        android:text="Enviar"
+        android:textSize="30sp"
+        tools:ignore="TextSizeCheck" />
+
+</LinearLayout>
